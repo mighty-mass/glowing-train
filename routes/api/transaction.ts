dceac2a5d3edfbcc68b4febd5d224de3ffc5fb22
Marco Massetti <marco.massetti@zenseact.com>
2025-11-30 23:13:54 +0100

    Start Fresh + PWA init

--- routes/api/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { HandlerContext } from "$fresh/server.ts";
-import { getXataClient } from '../../xata.ts'
-
-export const handler = async (req: Request, _ctx: HandlerContext) => {
-    const xata = getXataClient();
-
-    return new Response("Transaction added!", { status: 200 });
-};
